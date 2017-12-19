3223d0824463f8fe9321161e807bff514c2cfae0
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2017-12-19 21:55:14 +0100

    [ADD] canvas on torus

--- 3D-Test/ThreeJsTest.js
+++ 3D-Test/ThreeJsTest.js
@@ -1,13 +1,56 @@
-// setup
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+// canvas
+// let width = window.innerWidth;
+let width = 256;
+// let height = window.innerHeight / 2;
+let height = 128;
+let size = 256;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let scene, camera, renderer, texture, geometry, material, torus;
 
-// adding cube
+function changeCanvas() {
+  ctx.font = '20pt Arial';
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(10, 10, canvas.width - 20, canvas.height - 20);
+  ctx.fillStyle = 'black';
+  ctx.textAlign = 'center';
+  ctx.textBaseline = 'middle';
+  ctx.fillText(new Date().getTime(), canvas.width / 2, canvas.height / 2);
+}
+
+// setup
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  // adding torus
+  texture = new THREE.Texture(canvas);
+  geometry = new THREE.TorusGeometry(2, 0.5, 20, 20);
+  material = new THREE.MeshBasicMaterial({ map: texture });
+  torus = new THREE.Mesh(geometry, material);
+  scene.add(torus);
+  camera.position.z = 5;
+}
+
+// animate
+function animate() {
+  requestAnimationFrame(animate);
+  changeCanvas();
+  texture.needsUpdate = true;
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+
+init();
+animate();
