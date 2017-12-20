2010997ad40d0ccdce6e8fc57068806766643375
Eirik Halvard Sæther <eirik.halvard.95@gmail.com>
2017-12-20 23:50:37 +0100

    [REFACTOR] small optimalizations

--- Game-of-life/game.js
+++ Game-of-life/game.js
@@ -1,90 +1,190 @@
-let canvasWidth = 512;
-let canvasHeight = 128;
-let windowWidth = window.innerWidth / 2;
-let windowHeight = window.innerHeight;
+const canvasWidth = 512;
+const canvasHeight = 128;
 let resolution = 4;
-let generation;
 let fps = 30;
 let canvas, scene, camera, renderer, texture, geometry, material, torus;
-
-let genCount = 0;
-let genCountDom;
-let deathCount = 0;
-let deathCountDom;
-let deathPerGenDom;
-
-let deathColor;
-let aliveColor;
-
+let genCount, deathCount, genCountDom, deathCountDom, deathPerGenDom;
+let deathColor, aliveColor;
+let generation;
 let id;
 let is3D;
 
-function set3D() {
-  is3D = true;
+// SETUP
+function setup() {
+  addEventHandlers();
+  setColorScheme(0);
   resetStats();
   noLoop();
-  let width = canvasWidth / resolution;
-  let height = canvasHeight / resolution;
+  is3D = true;
 
-  resizeCanvas(canvasWidth, canvasHeight);
+  let width = floor(canvasWidth / resolution);
+  let height = floor(canvasHeight / resolution);
+  createCanvas(width * resolution, height * resolution);
+
+  canvas = document.getElementById('defaultCanvas0');
   canvas.style.display = 'none';
+  document.getElementById('canvasPlacement').appendChild(canvas);
 
   generation = createGrid(width, height);
-  randomizeGrid(generation);
+  randomizeGrid();
   renderCanvas();
 
   init();
   animate();
 }
+function addEventHandlers() {
+  genCountDom = document.getElementById('genCount');
+  deathCountDom = document.getElementById('deathCount');
+  deathPerGenDom = document.getElementById('deathPerGen');
+  document.getElementById('fps-range').oninput = function() {
+    fps = this.value;
+  };
+  document.getElementById('colorButton1').onchange = function() {
+    setColorScheme(0);
+  };
+  document.getElementById('colorButton2').onchange = function() {
+    setColorScheme(1);
+  };
+  document.getElementById('colorButton3').onchange = function() {
+    setColorScheme(2);
+  };
+  document.getElementById('dimensionInput').onchange = function() {
+    if (this.checked) {
+      set3D();
+    } else {
+      set2D();
+    }
+  };
+  document.getElementById('higherResolution').onclick = function() {
+    if (
+      document.getElementById('lowerResolution').classList.contains('disabled')
+    ) {
+      document.getElementById('lowerResolution').classList.remove('disabled');
+    }
+    resolution /= 2;
+    if (is3D) {
+      set3D();
+    } else {
+      set2D();
+    }
 
-function set2D() {
-  is3D = false;
-  resetStats();
-  cancelAnimationFrame(id);
-  resizeCanvas(windowWidth, windowHeight);
-  canvas.style.display = 'block';
-  renderer.domElement.style.display = 'none';
-  let width = floor(windowWidth / resolution);
-  let height = floor(windowHeight / resolution);
+    if (resolution == 2) {
+      this.classList.add('disabled');
+    }
+  };
+  document.getElementById('lowerResolution').onclick = function() {
+    if (
+      document.getElementById('higherResolution').classList.contains('disabled')
+    ) {
+      document.getElementById('higherResolution').classList.remove('disabled');
+    }
+    resolution *= 2;
+    if (is3D) {
+      set3D();
+    } else {
+      set2D();
+    }
 
-  generation = createGrid(width, height);
-  randomizeGrid(generation);
-  renderCanvas();
+    if (resolution > 100) {
+      this.classList.add('disabled');
+    }
+  };
 
-  loop();
+  document.getElementById('randomize').onclick = function() {
+    resetStats();
+    randomizeGrid();
+  };
+}
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / 2 / window.innerHeight,
+    0.1,
+    1000
+  );
+  renderer = new THREE.WebGLRenderer({ alpha: true });
+  renderer.setSize(window.innerWidth / 2, window.innerHeight);
+  document.getElementById('canvasPlacement').appendChild(renderer.domElement);
+
+  // adding torus
+  texture = new THREE.Texture(canvas);
+  geometry = new THREE.TorusGeometry(25, 10, 30, 30);
+  material = new THREE.MeshBasicMaterial({ map: texture });
+  torus = new THREE.Mesh(geometry, material);
+  scene.add(torus);
+  camera.position.z = 75;
+
+  // controls
+  let controls = new THREE.OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 1, 0);
+  controls.update();
 }
 
+// LOOPS
+function animate() {
+  setTimeout(function() {
+    advanceGeneration();
+    updateStats();
+  }, 1000 / fps);
+  id = requestAnimationFrame(animate);
+  texture.needsUpdate = true;
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
 function draw() {
   frameRate(fps);
   advanceGeneration();
   updateStats();
 }
 
-function setup() {
-  setColorScheme(0);
-  genCountDom = document.getElementById('genCount');
-  deathCountDom = document.getElementById('deathCount');
-  deathPerGenDom = document.getElementById('deathPerGen');
-
-  resetStats();
+// SWITCHING BETWEEN 2D AND 3D
+function set3D() {
   noLoop();
+  resetStats();
   is3D = true;
-  canvasWidth -= canvasWidth % resolution;
-  canvasHeight -= canvasHeight % resolution;
-  let width = canvasWidth / resolution;
-  let height = canvasHeight / resolution;
-  createCanvas(canvasWidth, canvasHeight);
-  canvas = document.getElementById('defaultCanvas0');
+
+  let width = floor(canvasWidth / resolution);
+  let height = floor(canvasHeight / resolution);
+  resizeCanvas(width * resolution, height * resolution);
   canvas.style.display = 'none';
-  document.getElementById('canvasPlacement').appendChild(canvas);
+  renderer.domElement.style.display = 'block';
 
   generation = createGrid(width, height);
-  randomizeGrid(generation);
+  randomizeGrid();
   renderCanvas();
-  init();
+
   animate();
 }
+function set2D() {
+  cancelAnimationFrame(id);
+  resetStats();
+  is3D = false;
 
+  let width = floor(window.innerWidth / 2 / resolution);
+  let height = floor(window.innerHeight / resolution);
+
+  resizeCanvas(width * resolution, height * resolution);
+  canvas.style.display = 'block';
+  renderer.domElement.style.display = 'none';
+
+  generation = createGrid(width, height);
+  randomizeGrid();
+  renderCanvas();
+
+  loop();
+}
+
+// CREATING A NEW GAME
+class Square {
+  constructor(x, y, value) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.nextValue = value;
+  }
+}
 function createGrid(width, height) {
   let arr = new Array(width);
   for (let i = 0; i < arr.length; i++) {
@@ -92,15 +192,19 @@ function createGrid(width, height) {
   }
   return arr;
 }
-
-function randomizeGrid(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      arr[i][j] = new Square(i, j, floor(random(2)));
+function randomizeGrid() {
+  for (let i = 0; i < generation.length; i++) {
+    for (let j = 0; j < generation[i].length; j++) {
+      generation[i][j] = new Square(i, j, floor(random(2)));
     }
   }
 }
 
+// ADVANCE TO NEXT GENERATION
+function advanceGeneration() {
+  createNextGeneration(generation);
+  renderCanvas();
+}
 function createNextGeneration(gen) {
   // Create next generation by filling nextValue
   for (let i = 0; i < gen.length; i++) {
@@ -122,7 +226,14 @@ function createNextGeneration(gen) {
     }
   }
 }
-
+function renderCanvas() {
+  for (let i = 0; i < generation.length; i++) {
+    for (let j = 0; j < generation[i].length; j++) {
+      fill(generation[i][j].value ? aliveColor : deathColor);
+      rect(i * resolution, j * resolution, resolution, resolution);
+    }
+  }
+}
 function countNeighbours(arr, x, y) {
   let count = 0;
   for (let i = -1; i < 2; i++) {
@@ -136,142 +247,18 @@ function countNeighbours(arr, x, y) {
   return count;
 }
 
-function advanceGeneration() {
-  createNextGeneration(generation);
-  renderCanvas();
-}
-
-class Square {
-  constructor(x, y, value) {
-    this.x = x;
-    this.y = y;
-    this.value = value;
-    this.nextValue = value;
-  }
-}
-
-function renderCanvas() {
-  for (let i = 0; i < generation.length; i++) {
-    for (let j = 0; j < generation[i].length; j++) {
-      if (generation[i][j].value) {
-        fill(aliveColor);
-      } else {
-        fill(deathColor);
-      }
-      rect(i * resolution, j * resolution, resolution, resolution);
-    }
-  }
-}
-
-function init() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(
-    75,
-    windowWidth / windowHeight,
-    0.1,
-    1000
-  );
-  renderer = new THREE.WebGLRenderer({ alpha: true });
-  renderer.setSize(windowWidth, windowHeight);
-  document.getElementById('canvasPlacement').appendChild(renderer.domElement);
-
-  // adding torus
-  texture = new THREE.Texture(canvas);
-  geometry = new THREE.TorusGeometry(25, 10, 30, 30);
-  material = new THREE.MeshBasicMaterial({ map: texture });
-  torus = new THREE.Mesh(geometry, material);
-  scene.add(torus);
-  camera.position.z = 75;
-
-  // controls
-  let controls = new THREE.OrbitControls(camera, renderer.domElement);
-  controls.target.set(0, 1, 0);
-  controls.update();
-}
-
-function animate() {
-  setTimeout(function() {
-    advanceGeneration();
-    updateStats();
-  }, 1000 / fps);
-  id = requestAnimationFrame(animate);
-  texture.needsUpdate = true;
-  // torus.rotation.x += 0.01;
-  // torus.rotation.y += 0.01;
-  renderer.render(scene, camera);
-
-  // genCount++;
-}
-
+// MANAGING STATS
 function updateStats() {
   genCountDom.innerText = genCount++;
   deathCountDom.innerText = deathCount;
   deathPerGenDom.innerText = round(deathCount / genCount);
 }
-
 function resetStats() {
   genCount = 0;
   deathCount = 0;
 }
 
-document.getElementById('fps-range').oninput = function() {
-  fps = this.value;
-};
-document.getElementById('colorButton1').onchange = function() {
-  setColorScheme(0);
-};
-document.getElementById('colorButton2').onchange = function() {
-  setColorScheme(1);
-};
-document.getElementById('colorButton3').onchange = function() {
-  setColorScheme(2);
-};
-document.getElementById('dimensionInput').onchange = function() {
-  if (this.checked) {
-    set3D();
-  } else {
-    set2D();
-  }
-};
-
-document.getElementById('higherResolution').onclick = function() {
-  if (
-    document.getElementById('lowerResolution').classList.contains('disabled')
-  ) {
-    document.getElementById('lowerResolution').classList.remove('disabled');
-  }
-  resolution /= 2;
-  // if (is3D) {
-  //   set3D();
-  // } else {
-  //   set2D();
-  // }
-
-  if (resolution == 2) {
-    this.classList.add('disabled');
-  }
-  console.log(resolution);
-};
-
-document.getElementById('lowerResolution').onclick = function() {
-  if (
-    document.getElementById('higherResolution').classList.contains('disabled')
-  ) {
-    document.getElementById('higherResolution').classList.remove('disabled');
-  }
-  resolution *= 2;
-  // if (is3D) {
-  //   set3D();
-  // } else {
-  //   set2D();
-  // }
-
-  if (resolution > 100) {
-    this.classList.add('disabled');
-  }
-  console.log(resolution);
-};
-
+// UPDATING COLOR
 function setColorScheme(schemeId) {
   switch (schemeId) {
     case 0:
@@ -279,8 +266,8 @@ function setColorScheme(schemeId) {
       deathColor = color(21, 135, 10);
       break;
     case 1:
-      aliveColor = color(71, 105, 209);
-      deathColor = color(229, 143, 45);
+      aliveColor = color(229, 143, 45);
+      deathColor = color(71, 105, 209);
       break;
     case 2: {
       aliveColor = color(40, 183, 104);
